0c908ab1652b5dcfb3e9c72df38342cea3f2f0d6
EC2 Default User <ec2-user@ip-172-31-39-200.us-east-2.compute.internal>
2019-03-22 21:31:57 +0000

    Update Gemfile for Heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
+  get 'films/show'
+
+  get 'films/index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'application#hello'
+  root 'films#index'
 
 end
